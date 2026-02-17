6d859fac91f5c1891fb86ad28f55d88afc8a6a3a
hamidashah <shah.hameeda@gmail.com>
2026-02-16 20:46:41 -0500

    Week 3 Assignment

--- assignment3.py
+++ assignment3.py
@@ -1,9 +1,84 @@
 import argparse
+import urllib.request
+import csv
+import io
+import re
 # other imports go here
+
+def downloadData(url):
+    """Downloads the data"""
+    """
+       Reads data from a URL and returns the data as a string
+
+       :param url:
+       :return: the content of the URL
+       """
+    # read the URL
+    with urllib.request.urlopen(url) as response:
+        response = response.read().decode('utf-8')
+
+    # return the data
+    return response
+
 
 
 def main(url):
     print(f"Running main with URL = {url}...")
+    results = downloadData(url)
+    url_data = downloadData(url)
+
+    csv_data = csv.reader(io.StringIO())
+    for row in csv_data:
+        path_file = row[0]
+        datetime_access = row[1]
+        browser = row[2]
+        status_request = row[3]
+        size_bytes = row[4]
+
+    #processing the image hits
+    image= 0
+    total_images = 0
+
+    image_hits = re.compile(r"\.(jpg|gif|png)$")
+
+    with open("weblog.csv" , encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            total_images += 1
+            path_file = row[0]
+            if image_hits.search(path_file):
+                image += 1
+    percentage_wise = (image / total_images) * 100
+    print(f"Image requests account for {percentage_wise}% of all requests")
+
+    #processing the browser hits
+    Firefox = 0
+    Chrome = 0
+    Internet_explorer = 0
+    Safari = 0
+    User_agent = str(row[2])
+
+
+    if re.search(r"Firefox", User_agent):
+        Firefox +=1
+    elif re.search(r"Chrome", User_agent):
+        Chrome +=1
+    elif re.search(r"Internet Explorer", User_agent):
+        Internet_explorer +=1
+    elif re.search(r"Safari", User_agent):
+        Safari +=1
+
+    # most popular browser
+    most_popular= {
+        "Firefox": Firefox,
+        "Chrome": Chrome,
+        "Internet Explorer": Internet_explorer,
+        "Safari": Safari,
+    }
+
+    popular_browser= max(most_popular, key=most_popular.get)
+    print(f"The most popular browser is {popular_browser} ")
+
 
 
 if __name__ == "__main__":
